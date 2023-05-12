cfde7ee3d0b7962ea8cea9791bbf5ffb1e6ead24
Bettina Riedl <riedl.bettina@freenet.de>
2023-05-12 13:17:35 +0200

    run_simulation: add first draft

---
+++ src/run_simulation_ipu.py
@@ -0,0 +1,21 @@
+import os
+
+from schnetpack.md.simulation_hooks import LangevinThermostat
+from src.constants import WORKDIR
+from src.ethanol_simulation import EthanolSimulation
+
+thermostats = [
+#    LangevinThermostat(10, 10),
+    LangevinThermostat(300, 10)
+#    LangevinThermostat(500, 10),
+#    LangevinThermostat(1000, 10)
+]
+
+log_files = [
+#    os.path.join(WORKDIR, "log_thermostat_10"),
+    os.path.join(WORKDIR, "log_thermostat_300")
+#    os.path.join(WORKDIR, "log_thermostat_500"),
+#    os.path.join(WORKDIR, "log_thermostat_1000")
+]
+
+EthanolSimulation(thermostats, log_files, "ipu", 100000)
