e6c282299a2614c1e31e7f79f3eba417c58dcaad
Bettina Riedl <riedl.bettina@freenet.de>
2023-05-13 13:35:59 +0200

    add datetime and torch.device to executor

--- src/run_simulation_ipu.py
+++ src/run_simulation_ipu.py
@@ -1,4 +1,7 @@
+import datetime
 import os
+
+import torch
 
 from schnetpack.md.simulation_hooks import LangevinThermostat
 from constants import WORKDIR
@@ -18,4 +21,45 @@ log_files = [
 #    os.path.join(WORKDIR, "log_thermostat_1000")
 ]
 
-EthanolSimulation(thermostats, log_files, "ipu", 100000)
+start_ipu = datetime.datetime.now()
+
+EthanolSimulation(thermostats, log_files, torch.device("ipu"), 100000, True)
+
+end_ipu = datetime.datetime.now()
+
+start_ipu_2 = datetime.datetime.now()
+
+EthanolSimulation(thermostats, log_files, torch.device("ipu"), 100000, False)
+
+end_ipu_2 = datetime.datetime.now()
+
+start_cpu = datetime.datetime.now()
+
+EthanolSimulation(thermostats, log_files, torch.device("cpu"), 100000, True)
+
+end_cpu = datetime.datetime.now()
+
+start_cpu_2 = datetime.datetime.now()
+
+EthanolSimulation(thermostats, log_files, torch.device("cou"), 100000, False)
+
+end_cpu_2 = datetime.datetime.now()
+
+
+result = f"--------------------" \
+         f"Dauer Cmd: 'EthanolSimulation(thermostats, log_files, \"cpu\", 100000, True)'" \
+         f"" \
+         f"{start_cpu - end_cpu}" \
+         f"" \
+         f"Dauer Cmd: 'EthanolSimulation(thermostats, log_files, \"ipu\", 100000, True)'" \
+         f"" \
+         f"{start_ipu - end_ipu}" \
+         f"" \
+         f"Dauer Cmd: 'EthanolSimulation(thermostats, log_files, \"cpu\", 100000, False)'" \
+         f"" \
+         f"{start_cpu_2 - end_cpu_2}" \
+         f"" \
+         f"Dauer Cmd: 'EthanolSimulation(thermostats, log_files, \"ipu\", 100000, False)'" \
+         f"" \
+         f"{start_ipu_2 - end_ipu_2}" \
+         f""
