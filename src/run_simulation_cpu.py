133a217dcd5f346a947379d66d0d64470a6589dd
Bettina Riedl <riedl.bettina@freenet.de>
2023-05-28 15:08:44 +0200

    add run_simulation_cpu

---
+++ src/run_simulation_cpu.py
@@ -0,0 +1,25 @@
+import datetime
+import os
+
+import torch
+
+from schnetpack.md.simulation_hooks import LangevinThermostat
+from constants import WORKDIR
+from ethanol_simulation import EthanolSimulation
+
+thermostats = [
+#    LangevinThermostat(10, 10),
+    LangevinThermostat(300, 10)
+#    LangevinThermostat(500, 10),
+#    LangevinThermostat(1000, 10)
+]
+
+log_files = [
+#    os.path.join(WORKDIR, "log_thermostat_10"),
+    os.path.join(WORKDIR, "log_thermostat_300")
+#    os.path.join(WORKDIR, "log_thermostat_500"),
+#    os.path.join(WORKDIR, "log_thermostat_1000")
+]
+
+
+EthanolSimulation(thermostats, log_files, torch.device("cpu"), 5, False)
