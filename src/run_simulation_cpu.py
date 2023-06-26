40286ff945510de9b0d1ac8b982618c31074a265
Bettina Riedl <riedl.bettina@freenet.de>
2023-06-26 09:27:25 +0200

    use calculator subclass for cpu calculations

--- src/run_simulation_cpu.py
+++ src/run_simulation_cpu.py
@@ -1,13 +1,12 @@
 import datetime
 import os
 
-from schnetpack.md.calculators import SchNetPackCalculator
-
 import torch
 
 from schnetpack.md.simulation_hooks import LangevinThermostat
 from constants import WORKDIR
 from ethanol_simulation import EthanolSimulation
+from SchNetPackCalcIpu import SchNetPackCalcCPU
 
 thermostats = [
     LangevinThermostat(300, 10)
@@ -18,5 +17,5 @@ log_files = [
 ]
 
 ethanol_simulation = EthanolSimulation(
-    thermostats, log_files, torch.device("cpu"), SchNetPackCalculator)
+    thermostats, log_files, torch.device("cpu"), SchNetPackCalcCPU)
 ethanol_simulation.start_simulation(10)
